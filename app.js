c6e734e4a3e71eadcda8b5d73438d44e99d5caa9
yunaichun <yunaichun@qingting.fm>
2020-12-11 20:11:37 +0800

    feat: 修改 static 目录

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const static = require('koa-static');
 
 const app = new Koa();
 
-// == 可通过地址访问: http://localhost:4000/_book/index.html
-app.use(static(path.join(__dirname,  './')));
+// == 可通过地址访问: http://localhost:4000/index.html
+app.use(static(path.join(__dirname,  './_book')));
 
 app.listen(4000, () => {
   console.log('server started on port 4000');
